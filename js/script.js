083002c84bcfa5a2b1624ec4031ca111271fadc0
Łukasz Furmańczyk <furman2004@wp.pl>
2024-02-21 08:25:57 +0100

    computerMove else-if

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+​
+console.log('Wylosowana liczba to: ' + randomNumber);
+​
+let computerMove = 'nieznany ruch';
+​
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+    computerMove = 'papier';
+} else if (randomNumber == 3){
+    computerMove = 'nożyce';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-let playerMove = 'papier';
-printMessage('zagrałem' + playerMove '!Jeśli twój ruch to nożyce, to wygrywasz!')
+​
+printMessage('Mój ruch to: ' + computerMove);
+​
