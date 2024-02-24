67f33f4dba2d751526cd05db1bf157324784331e
Łukasz Furmańczyk <furman2004@wp.pl>
2024-02-24 19:23:17 +0100

    button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function playGame(argPlayerInput){
-  
+  let argPlayerInput= '3' ;
   function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -13,7 +13,7 @@ function playGame(argPlayerInput){
     }
   }
   function displayResult(argComputerMove, argPlayerMove){
-    
+    console.log('moves:', argComputerMove, argPlayerMove);
     if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
       printMessage( 'Ty wygrywasz!');
     } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
@@ -38,17 +38,26 @@ function playGame(argPlayerInput){
   let computerMove = getMoveName(randomNumber);
   
  printMessage('Mój ruch to: ' + computerMove);
-
+/*
   let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
+*/
   console.log('Gracz wpisał: ' + playerInput);
 
   let playerMove = getMoveName(playerInput);
  
   printMessage('Twój ruch to: ' + playerMove);
   
-  displayResult(argComputerMove, argPlayerMove);
+   displayResult(argComputerMove, argPlayerMove);
 
+   document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('Wybrano kamień');
+  });
+  document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('Wybrano papier');
+  });
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('Wybrano nożyce');
+  });
 }
-playGame(3);
+
 
