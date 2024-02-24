a9d50d7ff776af2019f0190cd523d79f01e75d1f
Łukasz Furmańczyk <furman2004@wp.pl>
2024-02-24 14:07:50 +0100

    one more new function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+function playGame(argPlayerInput){
+  
   function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -28,52 +29,26 @@
     } else{
       printMessage('Ty przegrywasz');
     }
-  }   
+  } 
+  
   let randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
   let computerMove = getMoveName(randomNumber);
-  /*
-  if(randomNumber == 1){
-    computerMove = 'kamień';
-  } else if (randomNumber == 2){
-      computerMove = 'papier';
-  } else if (randomNumber == 3){
-      computerMove = 'nożyce';
-  }*/
-
-  printMessage('Mój ruch to: ' + computerMove);
+  
+ printMessage('Mój ruch to: ' + computerMove);
 
   let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
   console.log('Gracz wpisał: ' + playerInput);
 
   let playerMove = getMoveName(playerInput);
-  /*
-  if(playerInput == '1'){
-    playerMove = 'kamień';
-  } else if (playerInput == '2'){
-    playerMove = 'papier';
-  } else if (playerInput == '3'){
-    playerMove = 'nożyce';
-  }*/
-
+ 
   printMessage('Twój ruch to: ' + playerMove);
-  /*
-  if(computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis');
-  } else if (computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('remis');
-  } else if (computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz');
-  } else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('remis');
-  } else{
-    printMessage('Ty przegrywasz');
-  }*/
+  
+  displayResult(argComputerMove, argPlayerMove);
+
+}
+playGame(3);
 
